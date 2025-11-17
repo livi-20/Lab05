e253a18bfff7ed9e3b407d31c667862f32531b2a
livi_20 <liviacorrales05Qgmail.com>
2025-11-17 00:45:51 +0000

    include dyn.h

--- lab5/include/dynarray.h
+++ lab5/include/dynarray.h
@@ -2,4 +2,20 @@
 #define DYNARRAY_H
 #include <stddef.h>
 
-typedef
+typedef struct {
+    int *data;
+    size_t size;
+    size_t capacidad;
+} DynArray;
+
+DynArray *crear_dyn(size_t inicial);
+
+void destroy(DynArray *arr);
+
+int push(DynArray *arr, int valor);
+
+int remove(DynArray *arr, size_t index);
+
+void print(const DynArray *arr);
+
+#endif
